602850a0550bd201edaf2512bb16b0fed7f38069
bhujo <utkarshapandey2004@gmail.com>
2023-11-13 15:13:58 +0530

    minor changes

--- app.js
+++ app.js
@@ -18,6 +18,15 @@ app.get('/',(req,res)=>{
     const params = {}
     res.status(200).render('index.pug',params);
 })
+app.get('/contact',(req,res)=>{
+    const params = {}
+    res.status(200).render('contact.pug',params);
+})
+app.get('/login',(req,res)=>{
+    const params = {}
+    res.status(200).render('login.pug',params);
+})
+
 
 // START THE SERVER
 app.listen(port,()=>{

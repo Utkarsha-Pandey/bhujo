5cc82b09e063a7b947d7c2a258bb49ba7d096845
Utkarsha Pandey <148997801+Utkarsha-Pandey@users.noreply.github.com>
2025-09-28 12:14:54 +0530

    real time updation no refresh needed

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,59 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
-import DashBoard from './pages/dashboard'
-import Signup from './pages/signup';
-import Login from './pages/signin';
-import UserProfile from './pages/profile';
+import React, { useEffect } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import DashBoard from "./pages/dashboard";
+import Signup from "./pages/signup";
+import Login from "./pages/signin";
+import UserProfile from "./pages/profile";
+import { io } from "socket.io-client";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-
-
+// Connect to backend Socket.IO server
+const socket = io("http://localhost:8000");
 
 function App() {
+  useEffect(() => {
+    // Listen for notifications from server
+    socket.on("notification", (data) => {
+      toast.info(data.message, {
+        position: "top-right",
+        autoClose: 3000,
+      });
+    });
+
+    // Cleanup listener on unmount
+    return () => {
+      socket.off("notification");
+    };
+  }, []);
+
   return (
     <>
       <Routes>
-        {/* Add a route for the root path */}
-
+        {/* Redirect root to signin */}
         <Route path="/" element={<Navigate to="/signin" />} />
-        <Route path="/dashboard" element={
-          <ProtectedRoutes>
-            <DashBoard />
-          </ProtectedRoutes>
-        }
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoutes>
+              <DashBoard />
+            </ProtectedRoutes>
+          }
         />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/signin' element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Login />} />
         <Route path="/profile/:userId" element={<UserProfile />} />
       </Routes>
+
+      {/* Toast notifications container */}
+      <ToastContainer />
     </>
   );
 }
-export function ProtectedRoutes(props) {
+
+export function ProtectedRoutes({ children }) {
   if (localStorage.getItem("user")) {
-    return props.children;
+    return children;
   } else {
     return <Navigate to="/signin" />;
   }
